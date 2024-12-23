4005f083b45723027ddfd131caf6d16c752b66af
Ananthaiyan Pillai <ananthpillai789@gmail.com>
2024-12-23 17:15:19 +0530

    demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Hero } from '../components/sections/Hero';
 import { HowItWorks } from '../components/sections/HowItWorks';
 import { Features } from '../components/sections/Features';
 import { FAQ } from '../components/sections/FAQ';
-import { Dashboard } from './Dashboard';
+
 
 export function Home() {
   return (
